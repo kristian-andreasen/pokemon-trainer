864bedbf3ad08d3596c0f338f90d5e4d2903ddcc
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-02-05 22:34:57 +0100

    displaying images of pokemons on trainer page

--- src/app/trainer/trainer.component.ts
+++ src/app/trainer/trainer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/models/pokemon.model';
+import { PokemonCatalogueService } from '../services/pokemon-catalogue.service';
 import { TrainerService } from '../services/trainer.service';
 
 @Component({
@@ -9,7 +10,17 @@ import { TrainerService } from '../services/trainer.service';
 })
 export class TrainerComponent implements OnInit {
   pokemons: Pokemon[] = [];
-  constructor(public trainerService: TrainerService) {}
+  constructor(
+    public trainerService: TrainerService,
+    private pokemonCatalogueService: PokemonCatalogueService) {}
+
+    // Method to get the URL of the Pokemon's image
+    getPokemonImageUrl(url: string): string {
+      // Extract the Pokemon's ID from the URL
+      const id = this.pokemonCatalogueService.extractID(url);
+      // Return the URL of the Pokemon's image
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    }
 
   ngOnInit(): void {
     this.trainerService.getTrainerPokemons()
